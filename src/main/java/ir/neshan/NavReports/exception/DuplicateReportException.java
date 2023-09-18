bcd51554a9eb48c5aab0894fd7862a4b4e63b55c
amir kayyal <amir.kayyal@gmail.com>
2023-09-18 12:22:20 +0330

    add DuplicateReportException

---
+++ src/main/java/ir/neshan/NavReports/exception/DuplicateReportException.java
@@ -0,0 +1,7 @@
+package ir.neshan.NavReports.exception;
+
+public class DuplicateReportException extends RuntimeException {
+    public DuplicateReportException(String message) {
+        super(message);
+    }
+}
